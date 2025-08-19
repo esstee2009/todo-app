f16ca734f859e86408897a8a4991f0d64c34fab6
esstee2009 <samitariq69@googlemail.com>
2025-08-19 22:30:22 +0100

    nsg-added (#1)

---
+++ modules/azurerm_nsg/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.name_nsg
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Umar"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "ssh"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
